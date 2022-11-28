bd2b9548529542c316b7613dbd23189309524e87
afianasrin1 <afia.nasrin3e@gmail.com>
2022-11-28 10:45:16 +0600

    product add section are complete

--- src/Pages/Dashboard/AllReports/AllReports.js
+++ src/Pages/Dashboard/AllReports/AllReports.js
@@ -1,11 +1,20 @@
-import React from 'react';
+import { useQuery } from "@tanstack/react-query";
+import React from "react";
+import useTitle from "../../../hooks/useTitle";
+import SmallLoader from "../../Shared/Loader/SmallLoader";
 
 const AllReports = () => {
-    return (
-        <div>
-
-        </div>
-    );
+  useTitle("All Reports");
+  const { data: allReports, isLoading } = useQuery({
+    queryKey: ["reports"],
+    queryFn: () =>
+      fetch(`${process.env.REACT_APP_ApiUrl}reports`).then((res) => res.json()),
+  });
+  if (isLoading) {
+    return <SmallLoader />;
+  }
+  console.log(allReports);
+  return <div></div>;
 };
 
 export default AllReports;
